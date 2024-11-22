7fb27742aeeed2c97f7f81cd94caeb8a732453cd
swinnett-bearcloud <s.winnett@bearcloudstudios.com>
2024-11-22 14:04:07 -0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,6 @@ if ingredients_list and your_name:
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefrrot_response.json(), use_container_width=True)
+sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     
